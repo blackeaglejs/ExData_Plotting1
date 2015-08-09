c701df422b857efe26f27afd43fa4e4c831720f8
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-08-09 08:15:31 -0400

    Add download script

---
+++ DownloadData.R
@@ -0,0 +1,16 @@
+## This code will download the file, unzip the data, and then
+## transform it into requested plots. 
+
+## Set the working directory to the forked repository. 
+setwd("~/dev/datasciencecoursera/ExData_Plotting1/")
+
+## Download the file. Warning - this will take time if you have a 
+## slow internet connection. 
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="electricpowerdata.zip", method = "curl")
+
+unzip("electricpowerdata.zip")
+
+## We need the file name downloaded file for the next series of scripts.
+list.Files("*.txt")
